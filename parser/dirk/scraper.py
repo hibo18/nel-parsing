2cef76cfea2d294739db69c40dbe8e161cd1e6f2
Jusk <speranskiy.job@gmail.com>
2022-11-27 11:18:48 +0400

    method DirkScraper.get_products() can now selectively parse categories

--- parser/dirk/scraper.py
+++ parser/dirk/scraper.py
@@ -31,6 +31,13 @@ class DirkScraper:
             self,
             categories: Iterable[BeautifulSoup]
     ) -> List[dict]:
+        """
+            Converting bs4 html's to dict
+            
+            :categories: iterable, bs4 objects
+            
+            return list of categories as dict
+        """
         data = []
         for html_category in categories:
             html_link = html_category.find('a')
@@ -42,6 +49,13 @@ class DirkScraper:
         return data
 
     def get_categories(self, url_category: str) -> List[dict]:
+        """
+            Make get request to url, parse categories data
+
+            :url_category: url with categories
+
+            return list of categories as dict
+        """
         html_page = requests.get(url_category).text
         soup = BeautifulSoup(html_page, 'html.parser')
         soup = soup.find('nav', 'product-category-header__nav')
@@ -50,19 +64,27 @@ class DirkScraper:
         return categories
 
     def get_product(self, url_product: str) -> dict:
+        """
+            Make get request to url of product, parse data product
+
+            :url_product: product url
+
+            return list of categories as dict
+        """
         response = requests.get(url_product)
         html_product = BeautifulSoup(response.text, 'html.parser')
         product_name = html_product.find(
-                'div', 'product-details__info').find(
-                    'h1', 'product-details__info__title').text
+            'div', 'product-details__info').find(
+            'h1', 'product-details__info__title').text
         img_url = html_product.find(
-                'div', 'product-details__image').find('img')['src']
+            'div', 'product-details__image').find('img')['src']
         price_segment = html_product.find('div', 'product-card__price')
         euros = price_segment.find('span', 'product-card__price__euros').text
         cents = price_segment.find('span', 'product-card__price__cents').text
         product_price = euros + cents
         try:
-            product_old_price = price_segment.find('div', 'product-card__price__old').text
+            product_old_price = price_segment.find(
+                'div', 'product-card__price__old').text
         except AttributeError:
             logging.info(f'{product_name} is sold without discount.')
             product_old_price = product_price
@@ -73,10 +95,18 @@ class DirkScraper:
             'img_url': img_url,
             'price': product_price,
             'old_price': product_old_price.strip()
-            }
+        }
+        logging.info(f"{product_name} succesfully scraped!")
         return product
 
-    def collect_product_urls(self, url_subcategory: str) -> Iterable[BeautifulSoup]:
+    def collect_product_urls(self, url_subcategory: str) -> List[str]:
+        """
+            Make get request to url and collect all links to product
+
+            :url_subcategory: subcategory url
+
+            return list of urls
+        """
         html_page = requests.get(url_subcategory)
         soup = BeautifulSoup(html_page.text, 'html.parser')
         soup = soup.find(
@@ -92,25 +122,61 @@ class DirkScraper:
         ))
         return product_urls
 
-    def get_products(self) -> List[dict]:
+    def get_products(self,
+        to_category: str = None,
+        to_subcategory: str = None,
+        is_end: bool = False
+    ) -> List[dict]:
+        """
+            Main method to get all or selected products from category/subcategory.
+
+            :to_category: category name | Copy from site
+            :to_subcategory: subcategory name | Copy from site
+            :is_end: if false, skips only categories at the beginning
+                    if true, parses only the selected category
+
+            return list of products
+        """
+
         categories = self.get_categories(self.MAIN_CATALOG)
         products = []
 
         for category in categories:
-            logging.info(f"Handling {category['name']} category...")
-            category_url = category['link']
-            subcategories = self.get_categories(category_url)
+            try:
+                if category['name'] != to_category and to_category:
+                    logging.info(
+                        f"Going to {to_category}, category {category['name']} skipped!")
+                    continue
+                else:
+                    to_category = to_category if is_end else None
+                logging.info(f"Handling {category['name']} category...")
+                category_url = category['link']
+                subcategories = self.get_categories(category_url)
+            except AttributeError:
+                logging.warning(
+                    f"Category {category['name']} has no subcategory!")
+                continue
+
             for subcategory in subcategories:
+                if subcategory['name'] != to_subcategory and to_subcategory:
+                    logging.info(
+                        f"Going to {to_subcategory}, category {subcategory['name']} skipped!")
+                    continue
+                else:
+                    to_subcategory = to_subcategory if is_end else None
                 logging.info(f"Handling {subcategory['name']} subcategory...")
                 subcategory_url = subcategory['link']
-                products_url = self.collect_product_urls(
-                    subcategory_url
-                )
-                logging.info("Handling products...")
-                products_subcategory = list(map(
-                    lambda url: self.get_product(url),
-                    products_url
-                ))
-                products += products_subcategory
+                try:
+                    products_url = self.collect_product_urls(
+                        subcategory_url
+                    )
+                    logging.info("Handling products...")
+                    products_subcategory = list(map(
+                        lambda url: self.get_product(url),
+                        products_url
+                    ))
+                    products += products_subcategory
+                except AttributeError:
+                    logging.warning(
+                        f"Subcategory {subcategory['name']} has no products")
         return products
-            
